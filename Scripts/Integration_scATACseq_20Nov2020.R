a9fbbc89d2895df72436606dd23234bbfeb497c4
bansalvi <51865783+bansalvi@users.noreply.github.com>
2021-05-26 10:12:11 +0200

    Update Integration_scATACseq_20Nov2020.R

--- Scripts/Integration_scATACseq_20Nov2020.R
+++ Scripts/Integration_scATACseq_20Nov2020.R
@@ -462,7 +462,59 @@ sessionInfo()
 
 
 
+R version 3.6.0 (2019-04-26)
+Platform: x86_64-pc-linux-gnu (64-bit)
+Running under: Debian GNU/Linux 9 (stretch)
 
+Matrix products: default
+BLAS/LAPACK: /usr/lib/libopenblasp-r0.2.19.so
+
+locale:
+ [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8     LC_MONETARY=en_US.UTF-8    LC_MESSAGES=C             
+ [7] LC_PAPER=en_US.UTF-8       LC_NAME=C                  LC_ADDRESS=C               LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
+
+attached base packages:
+[1] parallel  stats4    stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+ [1] ggplot2_3.3.2          ballgown_2.5.3         GenomicFeatures_1.36.4 AnnotationDbi_1.46.0   Biobase_2.44.0         stringi_1.4.3          readxl_1.3.1          
+ [8] harmony_1.0            Rcpp_1.0.1             future_1.14.0          GenomicRanges_1.36.0   GenomeInfoDb_1.20.0    IRanges_2.18.1         S4Vectors_0.22.1      
+[15] BiocGenerics_0.30.0    Seurat_3.2.0.9015      Signac_1.1.0          
+
+loaded via a namespace (and not attached):
+  [1] reticulate_1.12             tidyselect_1.1.0            RSQLite_2.1.1               htmlwidgets_1.3             grid_3.6.0                 
+  [6] BiocParallel_1.18.0         Rtsne_0.15                  munsell_0.5.0               codetools_0.2-16            ica_1.0-2                  
+ [11] miniUI_0.1.1.1              withr_2.1.2                 colorspace_1.4-1            OrganismDbi_1.26.0          knitr_1.23                 
+ [16] rstudioapi_0.10             ROCR_1.0-7                  tensor_1.5                  listenv_0.7.0               GenomeInfoDbData_1.2.1     
+ [21] polyclip_1.10-0             bit64_0.9-7                 farver_2.0.3                vctrs_0.3.5                 generics_0.0.2             
+ [26] xfun_0.8                    biovizBase_1.32.0           lsa_0.73.2                  ggseqlogo_0.1               R6_2.4.0                   
+ [31] rsvd_1.0.1                  AnnotationFilter_1.8.0      bitops_1.0-6                spatstat.utils_1.13-0       reshape_0.8.8              
+ [36] DelayedArray_0.10.0         assertthat_0.2.1            promises_1.0.1              scales_1.0.0                nnet_7.3-12                
+ [41] gtable_0.3.0                npsurv_0.4-0                sva_3.32.1                  globals_0.12.4              goftest_1.1-1              
+ [46] ggbio_1.32.0                ensembldb_2.8.0             rlang_0.4.9                 genefilter_1.66.0           RcppRoll_0.3.0             
+ [51] splines_3.6.0               rtracklayer_1.44.0          lazyeval_0.2.2              acepack_1.4.1               dichromat_2.0-0            
+ [56] checkmate_1.9.4             BiocManager_1.30.4          reshape2_1.4.3              abind_1.4-5                 backports_1.1.4            
+ [61] httpuv_1.5.1                Hmisc_4.2-0                 RBGL_1.60.0                 tools_3.6.0                 ellipsis_0.2.0.1           
+ [66] gplots_3.0.4                RColorBrewer_1.1-2          ggridges_0.5.1              plyr_1.8.6                  base64enc_0.1-3            
+ [71] progress_1.2.2              zlibbioc_1.30.0             purrr_0.3.4                 RCurl_1.95-4.12             prettyunits_1.0.2          
+ [76] rpart_4.1-15                deldir_0.1-22               pbapply_1.4-0               cowplot_1.0.0               zoo_1.8-6                  
+ [81] SummarizedExperiment_1.14.1 ggrepel_0.8.1               cluster_2.0.8               magrittr_1.5                data.table_1.12.8          
+ [86] lmtest_0.9-37               RANN_2.6.1                  SnowballC_0.7.0             ProtGenerics_1.16.0         fitdistrplus_1.0-14        
+ [91] matrixStats_0.56.0          hms_0.5.3                   patchwork_1.0.1.9000        lsei_1.2-0                  mime_0.7                   
+ [96] xtable_1.8-4                XML_3.98-1.20               gridExtra_2.3               compiler_3.6.0              biomaRt_2.40.3             
+[101] tibble_3.0.1                KernSmooth_2.23-15          crayon_1.3.4                htmltools_0.3.6             mgcv_1.8-28                
+[106] later_0.8.0                 Formula_1.2-3               tidyr_1.1.0                 DBI_1.0.0                   tweenr_1.0.1               
+[111] MASS_7.3-51.4               Matrix_1.2-17               gdata_2.18.0                igraph_1.2.4.1              pkgconfig_2.0.2            
+[116] GenomicAlignments_1.20.1    foreign_0.8-71              plotly_4.9.0                annotate_1.62.0             XVector_0.24.0             
+[121] stringr_1.4.0               VariantAnnotation_1.30.1    digest_0.6.20               sctransform_0.2.0           RcppAnnoy_0.0.12           
+[126] graph_1.62.0                spatstat.data_1.4-0         Biostrings_2.52.0           cellranger_1.1.0            leiden_0.3.3               
+[131] fastmatch_1.1-0             htmlTable_1.13.1            uwot_0.1.8                  curl_3.3                    shiny_1.3.2                
+[136] Rsamtools_2.0.0             gtools_3.8.1                lifecycle_0.2.0             nlme_3.1-139                jsonlite_1.6               
+[141] limma_3.40.6                viridisLite_0.3.0           BSgenome_1.52.0             pillar_1.4.4                lattice_0.20-38            
+[146] GGally_1.4.0                httr_1.4.1                  survival_2.44-1.1           glue_1.4.2                  spatstat_1.60-1            
+[151] png_0.1-7                   bit_1.1-14                  ggforce_0.3.1               blob_1.2.1                  latticeExtra_0.6-28        
+[156] caTools_1.17.1.2            memoise_1.1.0               dplyr_1.0.2                 irlba_2.3.3                 future.apply_1.3.0         
+[161] ape_5.3                    
 
 
 
